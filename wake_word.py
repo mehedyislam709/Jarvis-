335ef1780110427bf2b7d9a60b46432ea289a894
mehedyislam709 <mehedyislam709@gmail.com>
2026-07-17 10:54:47 +0600

    Create wake_word.py

---
+++ wake_word.py
@@ -0,0 +1,19 @@
+class Microphone:
+    def record(self):
+        pass
+
+class WakeWordDetector:
+    def detect(self, audio):
+        pass
+
+class SpeechToText:
+    def transcribe(self, audio):
+        pass
+
+class TextToSpeech:
+    def speak(self, text):
+        pass
+
+class AIBrain:
+    def process_command(self, text):
+        pass
